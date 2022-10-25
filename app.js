2e690dff928bf92737f47b8d9e9a17f36efda4ef
Łukasz Wartałowicz <wartalowicz.lukasz.praca@gmail.com>
2022-10-25 19:28:15 +0200

    Add randChoice func and person loop

---
+++ app.js
@@ -0,0 +1,37 @@
+const os = require('os');
+const fs = require('fs');
+
+console.log('Platform: ', os.platform());
+console.log('Arch: ', os.arch());
+console.log('UserInfo', os.userInfo());
+
+const genders = ['female', 'male'];
+const femaleNames = ['Rebecca', 'Susan', 'Nancy', 'Margaret', 'Patricia', 'Jennifer'];
+const maleNames = ['Nicholas', 'Lucas', 'Jonathan', 'Patrick', 'Ethan'];
+const lastnames = ['Cox', 'Lee', 'Scott', 'Baker', 'Walker', 'Lewis'];
+const people = [];
+
+const randChoice = function (arr) {
+    const result = arr[Math.floor(Math.random() * arr.length)];
+    return result;
+}
+for (let i = 0; i <20; i++) {
+const person ={};
+
+person.gender = randChoice(genders);
+if (person.gender === 'femae') person.name = randChoice(femaleNames);
+else person.name = randChoice(maleNames);
+person.lastname = randChoice(lastnames);
+person.age = Math.floor(Math.random() * 100);
+
+people.push(person)
+}
+
+const data = JSON.stringify(people)
+
+fs.writeFile('people.txt', data, (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
+  });
+  
+  console.log(people.length);
